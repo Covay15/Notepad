d7575552dec3be55b0f66028fb3ff6626fabd7ca
Taiwo15 <taiwo.ibitoye@babbangona.com>
2023-03-23 23:16:01 +0100

    Creating a notepad. Incomplete

--- app/src/main/java/com/example/bgnotes/NewNoteFragment.kt
+++ app/src/main/java/com/example/bgnotes/NewNoteFragment.kt
@@ -6,11 +6,18 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.*
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.bgnotes.databinding.FragmentMyNoteBinding
 import com.example.bgnotes.databinding.FragmentNewNoteBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import room_database.Note
+import room_database.NoteDao
+import room_database.NotesDatabase
 
 class NewNoteFragment : Fragment() {
     override fun onCreateView(
@@ -26,12 +33,17 @@ class NewNoteFragment : Fragment() {
     private lateinit var binding: FragmentNewNoteBinding
     private var selectedImageUri: Uri? = null
     private lateinit var selectedImageView: ImageView
+    private lateinit var db: NotesDatabase
+    private lateinit var noteDao: NoteDao
     private lateinit var galleryLauncher: ActivityResultLauncher<String>
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNewNoteBinding.bind(view)
         selectedImageView=binding.imgNote
+        db = NotesDatabase.invoke(requireContext()) // initialize the db property here
+
 
         galleryLauncher =
             registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
@@ -43,6 +55,28 @@ class NewNoteFragment : Fragment() {
 
         binding.addImageBtn.setOnClickListener {
             galleryLauncher.launch("image/*")
+        }
+
+        binding.saveNoteBtn.setOnClickListener {
+            val title = binding.textTitle.text.toString()
+            val description = binding.textBody.text.toString()
+            val note = Note (title = title, description = description)
+
+            if (title.isEmpty() && description.isEmpty()) {
+                Toast.makeText(requireContext(), "Empty Note", Toast.LENGTH_SHORT).show()
+            } else {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val dao = db.getnoteDao().insert(note)
+                }
+                Toast.makeText(
+                    requireContext(),
+                    "Note has been saved successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.nav_container, MyNoteFragment()).addToBackStack(null).commit()
+            }
+
         }
     }
 }
